5a6f63cb2ac44ae1035dfda167459c0acba35469
Sukrit Bhatnagar <skrtbhtngr@gmail.com>
2017-09-23 00:35:04 +0530

    Updated link for auto-pairs

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-mv ~/.vimrc/.vimrc ~/.vimrc
+mv ~/.vimrc/.vimrc .vimrc
 
-#Install pathogen
+echo "Installing vim-pathogen..."
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+curl -Lso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
-#Install vim-plug
+echo "Installing vim-plug..."
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-#Install auto-pairs
-git clone git://github.com/jiangmiao/auto-pairs.git ~/.vim/bundle/auto-pairs
+echo "Installing auto-pairs"
+git clone https://github.com/jiangmiao/auto-pairs.git ~/.vim/bundle/auto-pairs
