e359e2bbd92efbc141606b4cbda8d6d068262e97
Sukrit Bhatnagar <skrtbhtngr@gmail.com>
2017-09-26 22:04:59 +0530

    minor updates

--- install.sh
+++ install.sh
@@ -12,4 +12,4 @@ git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
 echo -e "\nInstalling auto-pairs...\n"
 git clone https://github.com/jiangmiao/auto-pairs.git ~/.vim/bundle/auto-pairs
 
-echo "Done!"
+echo -e "\nDone!"
