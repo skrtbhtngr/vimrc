4be68500f20ad8959784554ae19dcf67f27c15e2
Sukrit Bhatnagar <skrtbhtngr@gmail.com>
2017-09-26 22:01:38 +0530

    minor updates

--- install.sh
+++ install.sh
@@ -2,14 +2,14 @@
 
 mv ~/.vimconf/.vimrc ~/.vimrc
 
-echo "Installing vim-pathogen..."
+echo -e "Installing vim-pathogen...\n"
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -Lo ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
-echo "Installing vim-airline"
+echo "\nInstalling vim-airline\n"
 git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
 
-echo "Installing auto-pairs"
+echo -e "\nInstalling auto-pairs\n"
 git clone https://github.com/jiangmiao/auto-pairs.git ~/.vim/bundle/auto-pairs
 
-echo "Done!\n"
+echo "Done!"
